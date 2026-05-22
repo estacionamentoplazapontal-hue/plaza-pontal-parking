681408b962a70e7db69ca539ddbf569ee0602ddd
Jhonny <estacionamentoplazapontal@gmail.com>
2026-05-21 21:05:14 -0300

    react app correto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        minHeight: "100vh",
+        padding: "40px",
+        color: "#000000",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>Plaza Pontal Parking</h1>
+
+      <p>
+        Sistema iniciado com sucesso.
+      </p>
+    </div>
+  );
+}
+
+export default App;
